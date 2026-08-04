25a54f967c00b520c53609bac1cf551a6e7b1410
YukihiroArakawa <yukiburos@gmail.com>
2026-08-04 18:41:22 +0900

    build(nix): add flake dev shell and direnv

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development environment for Blind 75 solutions";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          mdview = pkgs.writeShellApplication {
+            name = "mdview";
+            runtimeInputs = [ pkgs.mdserve ];
+            text = ''
+              if [ "$#" -eq 0 ]; then
+                echo "Usage: mdview <Markdown file or directory> [mdserve options]" >&2
+                exit 2
+              fi
+
+              exec mdserve --open --port "''${MDSERVE_PORT:-3001}" "$@"
+            '';
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              direnv
+              mdserve
+            ] ++ [ mdview ];
+          };
+        });
+    };
+}
